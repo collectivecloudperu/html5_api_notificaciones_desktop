8ee1affe603564d864bc40f6812a4ab89ec5a2e9
Collective Cloud Peru <collectivecloudperu@gmail.com>
2015-10-31 23:55:37 -0500

    Notificaciones Desktop con HTML5
    Aplicación con la API de Notificaciones Desktop en HTML5

---
+++ js/script.js
@@ -0,0 +1,44 @@
+﻿           
+// Validamos y activamos el Permiso para Notificar
+if(Notification.permission!=="granted") {
+	Notification.requestPermission();
+}
+
+// var notificador = document.getElementById("notificadorThingy");
+
+// Instanciamos el botón, para que al hacer Click en el, se proceda a lanzar la Notificación o un mensaje para actualizar el Navegador
+document.getElementById("btn_notificar").addEventListener("click", onNotificationButtonClick);
+
+// Validamos si el Navegador soporta las Notificaciones en HTML 5
+function onNotificationButtonClick() {
+	if (!isNotificationSupported()) {
+		logg("Tu navegador no soporta Notificaciones. Por favor, utiliza una versión Reciente del Navegador Google Chrome o Safari.");
+	return;
+	}
+
+	// Si el Navegador soporta las Notificaciones HTML 5, entonces que proceda a Notificar
+	var notificacion = new Notification("Tienes nuevos Mensajes !", {
+	    icon: 'img/gmail.png',
+	    body: 'Abrir Bandeja de Gmail.'
+	});
+
+	// Redireccionamos a un determinado Destino o URL al hacer click en la Notificación
+	notificacion.onclick = function() {
+		window.open("http://gmail.com/");
+	};					
+}
+		
+// Solicitamos los Permisos del Sistema
+function requestPermissions() {
+
+}
+
+// Luego del Permiso del Sistema, le indicamos que nos Muestre la Notificación
+function isNotificationSupported() {
+	return ("Notification" in window);
+}
+
+// Mostramos el Mensaje de la Notificación
+function logg(mensaje) {
+	notificador.innerHTML += "<p>"+mensaje+"</p>";
+}
